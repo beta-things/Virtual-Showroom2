e749d00e2f3cd0fcc65dc33bfd0662e1b78d572e
beta-things <53658721+beta-things@users.noreply.github.com>
2021-12-04 11:22:10 -0800

    QUOTE PAGE STUFF
    -NEW MODEL FOR ADDITIONAL LINE ITMES
    -QUOTE PAGE STYLE
    -PRICE CALCULATOR AND LOCALE STRING (ADD COMMAS TO BIG NUMBERS)

---
+++ api/models/QuoteLineItems.js
@@ -0,0 +1,55 @@
+/**
+ * QuoteLineItems.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    friendlyName:{
+      type: "string",
+      required: true,
+      example: "PTZ Pedastle"
+    },
+   
+    price:{
+      type: "number",
+      defaultsTo: 0.00,
+    },
+
+    partCode:{
+      type:"string",
+      defaultsTo: ""
+    },
+
+    partDescription:{
+      type: "string",
+      defaultsTo: ""
+    },
+
+   
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    AssociatedBuild:{
+      model:"builds",
+      required: true,
+      example: "2"
+    },
+
+  },
+
+};
+
--- assets/js/pages/quote.page.js
+++ assets/js/pages/quote.page.js
@@ -7,6 +7,8 @@ parasails.registerPage('quote', {
     templateWithSlots: undefined,
     build: undefined,
     loadingBuild: false,
+    priceTotal : 0,
+    quoteLineItems: [],
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -17,6 +19,9 @@ parasails.registerPage('quote', {
   },
   mounted: async function() {
     //…
+    this.pricesToDisplay();
+    this.updateTotalPrice();
+
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -27,6 +32,51 @@ parasails.registerPage('quote', {
       this.loadingBuild = true;
       window.location.replace("/showroom/"+sessionCode);
     },
+
+    updateTotalPrice: function(index){
+      if(index){
+        this.build.buildParts[index].price = parseFloat(this.build.buildParts[index].vuPrice);
+      }
+      this.priceTotal = 0;
+      for(var i = 0; i<this.build.buildParts.length; i++){
+        var partPrice = this.build.buildParts[i].price;
+        var mathEnt = parseFloat(partPrice);
+        this.priceTotal += mathEnt;
+        
+      }
+      this.priceTotal = Math.round(this.priceTotal * 100) / 100;
+
+      this.priceTotal = this.priceTotal.toLocaleString();
+  
+    },
+
+    pricesToDisplay: function(){
+      for(var i = 0; i<this.build.buildParts.length; i++){
+        this.build.buildParts[i].vuPrice = this.build.buildParts[i].price.toLocaleString();
+      }
+      this.$forceUpdate();
+    },
+
+    focusPrice: function(index){
+      console.log("focus price");
+      console.log(this.build.buildParts[index].vuPrice);
+      console.log(this.build.buildParts[index].price);
+      this.build.buildParts[index].vuPrice = this.build.buildParts[index].price; 
+      this.$forceUpdate();
+    },
+    
+    blurPrice: function(index){
+      console.log("blur price");
+      this.pricesToDisplay();
+      
+    },
+
+    
+
+    newQuoteLineItem: function(){
+     //add to quoteLineItems therefore increasing the number of extra line item forms on the page
+     this.quoteLineItems.push({partCode:undefined, friendlyName: undefined, partDescription: undefined});
+    }
 
   }
   
